f870fc620e70580431c8e658eeaba830bfc5d2ba
smartkeylogic <dreams.dmroads@gmail.com>
2025-02-16 22:29:38 +1100

    Updated web app with Ver 3 changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "46966286"
+    serviceWorkerVersion: "251756506"
   }
 });
